743008184e13acad52069679485169dd6c1fbc3b
justintjoa <justintoja@gmail.com>
2019-11-20 19:45:33 -0800

    added all data in differnet columns

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import java.util.List;
 
 public class FeatureCollection {
- 	private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
+ 	public List<Feature> features;
+	public Metadata metadata;
+	private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 	 public String type;
 	public static FeatureCollection fromJSON(String json) {
         try {
